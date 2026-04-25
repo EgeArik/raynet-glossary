b6c0f1d226e3e8471a3982b5defdb21be7438629
Ege Arik <egearik4143@gmail.com>
2026-04-25 20:31:05 +0300

    Developer tag have added

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
+# Developer  : Ege ARIK
 # Raynet Junior Tech Glossary 
 
 tech_dictionary = { 
 "Python": "A high-level programming language.", 
 "Git": "A distributed version control system.",
-"Firewall":"Network analyze and protection device. "
-"Docker":"Gives a oppurtionaty to work in isolated workspaces."
-"Subnet":"A subnet is a smaller part of a larger network used to organize traffic and improve security."
+"Firewall":"Network analyze and protection device. ",
+"Docker":"Gives a oppurtionaty to work in isolated workspaces.",
+"Subnet":"A subnet is a smaller part of a larger network used to organize traffic and improve security.",
 "Git": "A distributed version control system." ,
 "Class": "A blueprint used to create objects in programming.",
 "API": "A set of rules that allows different software to communicate with each other. ",
