16560f963b44647dbe4dfc295972f16f2fcbed55
KejSrynT <umutcan.x58x@gmail.com>
2026-04-25 19:59:46 +0300

    Three system terms added to dictionary.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 
 tech_dictionary = { 
 "Python": "A high-level programming language.", 
-"Git": "A distributed version control system." 
+"Git": "A distributed version control system.",
+"Firewall":"Network analyze and protection device. "
+"Docker":"Gives a oppurtionaty to work in isolated workspaces."
+"Subnet":"A subnet is a smaller part of a larger network used to organize traffic and improve security."
 } 
 
 def list_terms(): 
