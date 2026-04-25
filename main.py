b33ce28b27d6d021887a4dfa7a920a87814bfdd5
Ege Arik <egearik4143@gmail.com>
2026-04-25 20:05:41 +0300

    3 software terms added the dictionary

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 
 tech_dictionary = { 
 "Python": "A high-level programming language.", 
-"Git": "A distributed version control system." 
+"Git": "A distributed version control system." ,
+"Class": "A blueprint used to create objects in programming.",
+"API": "A set of rules that allows different software to communicate with each other. ",
+"Bug": "An error or flaw in a program that causes incorrect behavior.",
+"Algorithm" : "A step by step process to solve a problem.",
+"Loop" : "Runs the same code again and again. it has become uncontrollable"
 } 
 
 def list_terms(): 
